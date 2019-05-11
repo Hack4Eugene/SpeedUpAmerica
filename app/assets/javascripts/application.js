f4f5fbd8bc6a2d9300473066f5c7dac99958a513
Ryan Olds <ryanrolds@gmail.com>
2019-05-11 14:37:29 -0700

    Removed turbolinks and misc

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require turbolinks
 //= require chosen-jquery
 //= require bootstrap-datepicker
 //= require ndt.js
