055df202d21c2eaccf71d90c490e8ae0d6361bfd
Ryan Olds <ryanrolds@gmail.com>
2019-06-12 21:59:36 -0700

    Added structured logger and Loggly sender

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,6 @@
+require 'lograge'
+require 'logglier' 
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -50,11 +53,20 @@ Rails.application.configure do
   config.log_level = :info
 
   # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
+  #config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
-  #config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
-  config.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+  config.lograge.enabled = true
+  config.lograge.keep_original_rails_log = false
+  config.lograge.formatter = Lograge::Formatters::Raw.new
+  config.lograge.logger = ActiveSupport::TaggedLogging.new(Logger.new(STDOUT))
+  loggly = Logglier.new("https://logs-01.loggly.com/inputs/"+ENV["LOGGLY_TOKEN"]+"/tag/speedupamerica-v1", :format => :json, threaded: true)
+  config.lograge.logger.extend(ActiveSupport::Logger.broadcast(loggly))
+
+  # add time to lograge
+  config.lograge.custom_options = lambda do |event|
+    { env: Rails.env }
+  end
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
