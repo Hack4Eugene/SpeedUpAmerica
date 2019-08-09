4be9594e80e7c5f0e5e63b8c60e0f916e0ccefa4
Ryan Olds <ryanrolds@gmail.com>
2019-08-08 19:17:15 -0700

    Removed get_location_data

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,16 +3,6 @@ class HomeController < ApplicationController
     @submission = Submission.new
   end
 
-  def get_location_data
-    if params[:longitude].nil? || params[:longitude].nil?
-      render :json => { :error => "Invalid value for latitude or longitude" }, :status => 400
-      return
-    end
-
-    data = Submission.get_location_data(params)
-    render json: data
-  end
-
   def calculate_ping
     render nothing: true
   end
--- app/models/submission.rb
+++ app/models/submission.rb
@@ -276,41 +276,6 @@ class Submission < ActiveRecord::Base
     end
   end
 
-  def self.get_location_data(params)
-    Geocoder.configure(timeout: 15)
-    result = Geocoder.search("#{params[:latitude]}, #{params[:longitude]}").first
-
-    country = nil
-    region = nil
-    city = 'unknown'
-    postal_code = nil
-
-    if result.present? && result.country == "USA"
-      if result.city.present?
-        city = result.city
-      end
-
-      if result.postal_code.present?
-        postal_code = result.postal_code[0...5]
-      end
-
-      if result.country_code.present?
-        country = result.country_code
-      end
-
-      if result.state_code.present?
-        region = result.state_code
-      end
-    end
-
-    data = {
-      'address' => city,
-      'zip_code' => postal_code,
-      'country' => country.upcase,
-      'region' => region.upcase
-    }
-  end
-
   def self.to_csv(date_range)
     range = date_range.split(' - ')
     CSV.generate do |csv|
