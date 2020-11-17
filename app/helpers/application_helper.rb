0d5715caefaabd978f8c3e87916ecc3a5568d60f
Noah Brenner <noah@noahbrenner.com>
2020-11-17 15:39:06 -0800

    Create bare /about and /faq pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,11 @@
 module ApplicationHelper
 
-  def body_css_class
-    action_name == 'internet_stats' && 'in-stats' || ''
-  end
-
   def header_file_name
     action_name == 'embeddable_view' ? 'social_share' : 'header'
   end
 
   def body_css_class
-    return 'result-page' if action_name == 'result_page' || action_name == 'show'
+    return 'result-page' if action_name == 'result_page'
     return 'in-stats' if action_name == 'internet_stats'
     return ''
   end
